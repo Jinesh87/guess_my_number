1d577e77c46cec576b6765bdb42e1257a30ba83a
Jinesh87 <zodiacaus@gmail.com>
2024-12-20 13:43:06 +0530

    Adding Background Image and README

---
+++ README.md
@@ -0,0 +1,39 @@
+# Guess My Number Game 🎲
+
+A fun, interactive mobile game built with **React Native** as part of the [React Native - The Practical Guide](https://www.udemy.com/course/react-native-the-practical-guide/) course on Udemy.
+
+##  Overview
+The **Guess My Number Game** challenges the app to guess a number chosen by the user within a given range. The user provides feedback (higher or lower) to guide the app's guesses. This project demonstrates core React Native concepts, including state management, component structure, and responsive design.
+
+## Versions
+- **Node**: 22.11.0
+- **React**: 18.2.0
+- **React-Native** : 0.76.3
+
+
+## Dependencies
+
+- **[expo](https://expo.dev/)**: Development platform for React Native apps.
+- **[expo-linear-gradient](https://docs.expo.dev/versions/latest/sdk/linear-gradient/)**: provides a native React view that transitions between multiple colors in a linear direction.
+
+
+## Installation and Setup Instructions 
+
+Clone down this repository. You will need `node` and `npm` installed globally on your machine.  
+
+Installation:
+
+`npm install`   
+
+To Start Server:
+
+`npm start`  
+
+To Visit App:
+
+Scan the QR code with Expo Go (Android) or the Camera app (iOS)
+
+
+
+## Udemy 
+   * https://www.udemy.com/course/react-native-the-practical-guide/
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,8 +4,16 @@ function StartGameScreen() {
     return (
         <View style={styles.inputContainer}>
             <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false} />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonConainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonConainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+
+            </View>
+
         </View>
     );
 }
@@ -14,6 +22,8 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
     inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
         marginTop: 100,
         marginHorizontal: 24,
         padding: 16,
@@ -22,18 +32,24 @@ const styles = StyleSheet.create({
         elevation: 4,
         shadowColor: 'black',
         shadowOffset: { width: 0, height: 2 },
-        shadowRadius:6,
-        shadowOpacity:0.25
+        shadowRadius: 6,
+        shadowOpacity: 0.25
     },
-    numberInput:{
-        height:70,
-        width:70,
-        fontSize:35,
-        borderBottomColor:'#ddb52f',
-        borderBottomWidth:3,
+    numberInput: {
+        height: 70,
+        width: 70,
+        fontSize: 35,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 3,
         color: '#ddb52f',
-        marginVertical:8,
-        fontWeight:'bold',
-        textAlign:'center'
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    buttonsContainer: {
+        flexDirection: 'row'
+    },
+    buttonConainer:{
+        flex:1
     }
 });
