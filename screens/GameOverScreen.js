5864cd359ee7d0bae995805d9c9b6d47af9f9a34
Jinesh87 <zodiacaus@gmail.com>
2024-12-20 15:41:28 +0530

    Managing Colors Globally

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,8 @@
+import { Text } from "react-native";
 function GameOverScreen(){
-    
+    return (
+        <Text>Game Over Screen!</Text>
+    );
 }
 
 export default GameOverScreen;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,21 +1,52 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
-function StartGameScreen() {
-    return (
-        <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false} />
-            <View style={styles.buttonsContainer}>
-                <View style={styles.buttonConainer}>
-                    <PrimaryButton>Reset</PrimaryButton>
-                </View>
-                <View style={styles.buttonConainer}>
-                    <PrimaryButton>Confirm</PrimaryButton>
-                </View>
-
+import Colors from "../constants/Colors";
+function StartGameScreen({onPickNumber}) {
+    const [enteredNumber, setEnteredNumber] = useState('');
+  
+    function numberInputHandler(enteredText) {
+      setEnteredNumber(enteredText);
+    }
+  
+    function resetInputHandler() {
+      setEnteredNumber('');
+    }
+  
+    function confirmInputHandler() {
+      const chosenNumber = parseInt(enteredNumber);
+  
+      if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+        Alert.alert(
+          'Invalid number!',
+          'Number has to be a number between 1 and 99.',
+          [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+        );
+        return;
+      } 
+      onPickNumber(chosenNumber);
+    }
+return (
+    <View style={styles.inputContainer}>
+        <TextInput
+            style={styles.numberInput}
+            maxLength={2}
+            keyboardType="number-pad"
+            autoCapitalize="none"
+            autoCorrect={false}
+            value={enteredNumber} onChangeText={numberInputHandler} />
+        <View style={styles.buttonsContainer}>
+            <View style={styles.buttonConainer}>
+                <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+            </View>
+            <View style={styles.buttonConainer}>
+                <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
             </View>
 
         </View>
-    );
+
+    </View>
+);
 }
 
 export default StartGameScreen;
@@ -27,7 +58,7 @@ const styles = StyleSheet.create({
         marginTop: 100,
         marginHorizontal: 24,
         padding: 16,
-        backgroundColor: '#4e0329',
+        backgroundColor: Colors.primary700,
         borderRadius: 8,
         elevation: 4,
         shadowColor: 'black',
@@ -39,9 +70,9 @@ const styles = StyleSheet.create({
         height: 70,
         width: 70,
         fontSize: 35,
-        borderBottomColor: '#ddb52f',
+        borderBottomColor: Colors.accent500,
         borderBottomWidth: 3,
-        color: '#ddb52f',
+        color: Colors.accent500,
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center'
@@ -49,7 +80,7 @@ const styles = StyleSheet.create({
     buttonsContainer: {
         flexDirection: 'row'
     },
-    buttonConainer:{
-        flex:1
+    buttonConainer: {
+        flex: 1
     }
 });
